7b42cf3f7553ba3dacf2e9c9423aacc79d6e4969
ycohn <cohn.yehuda@gmail.com>
2016-02-18 17:56:48 -0500

    component for new building

--- app/router.js
+++ app/router.js
@@ -2,26 +2,26 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+ location: config.locationType
 });
 
 Router.map(function() {
-  this.route('users', function() {
-    this.route('new');
+ this.route('users', function() {
+   this.route('new');
 
-    this.route('user', {
-      path: ':user_id'
-    });
-  });
-  this.route('buildings', function() {
-    this.route('new');
-    this.route('building', {path: ':building_id'}, function() {
-      this.route('topics', function() {
-        this.route('new');
-        this.route('topic', {path: ':topic_id'});
-      });
-    });
-  });
+   this.route('user', {
+     path: ':user_id'
+   });
+ });
+ this.route('buildings', function() {
+   this.route('new');
+   this.route('building', {path: ':building_id'}), function() {
+     this.route('topics', function() {
+       this.route('topic', {path: ':topic_id'});
+       this.route('new');
+     });
+   };
+ });
 });
 
 export default Router;
