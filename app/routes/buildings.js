d5c46f9c816b7b8876686989a874242a54fa7224
ycohn <cohn.yehuda@gmail.com>
2016-02-18 17:19:10 -0500

    add component for building

--- app/routes/buildings.js
+++ app/routes/buildings.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addBuilding(){
+      let building = this.modelFor(this.routeName);
+      debugger;
+      building.save().then((savedBuilding) => {
+        this.transitionTo('buildings.building', savedBuilding);
+      });
+    }
+  }
 });
