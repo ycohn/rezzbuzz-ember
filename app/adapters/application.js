b66032594d386c56bb0089f22ae8f8beb308be1f
ycohn <cohn.yehuda@gmail.com>
2016-02-17 23:22:53 -0500

    add adapter and namespace

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,6 @@
-import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
 
-export default DS.RESTAdapter.extend({
+export default ActiveModelAdapter.extend({
+  // host: "http://localhost:3000",
+  namespace: "api/v1"
 });
