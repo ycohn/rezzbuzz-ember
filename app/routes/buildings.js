c78ab6cbcc68be5c0a652efd3b17864f32b46e73
leighsn <leighscherrer@gmail.com>
2016-02-19 09:39:26 -0500

    removed debugger

--- app/routes/buildings.js
+++ app/routes/buildings.js
@@ -7,7 +7,6 @@ export default Ember.Route.extend({
   actions: {
     addBuilding(){
       let building = this.modelFor(this.routeName);
-      debugger;
       building.save().then((savedBuilding) => {
         this.transitionTo('buildings.building', savedBuilding);
       });
